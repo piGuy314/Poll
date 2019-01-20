834bd7275836f6628cc720d517639981d6cd0900
Aaron Eichhorst <aaroneichhorst@gmail.com>
2019-01-19 20:36:56 -0600

    Added a home view, controller action and route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 
 Rails.application.routes.draw do
-  devise_for :installs
-  resources :links
-  get 'home/index'
+  root to: 'pages#home'
 
-  root 'home#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
