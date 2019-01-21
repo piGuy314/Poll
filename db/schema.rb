83313966c0aca4699af67892ac1a38b56aad6898
Aaron Eichhorst <aaroneichhorst@gmail.com>
2019-01-20 22:58:55 -0600

    Created contacts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190107042647) do
+ActiveRecord::Schema.define(version: 20190121042144) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "installs", force: :cascade do |t|
     t.string "email", default: "", null: false
