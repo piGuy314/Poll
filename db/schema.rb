ca701fa5ee95c9eb3a0d15f878a53f62c0676549
Aaron Eichhorst <aaroneichhorst@gmail.com>
2019-01-21 19:46:03 -0600

    Added membership table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190121180522) do
+ActiveRecord::Schema.define(version: 20190122013534) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(version: 20190121180522) do
   create_table "links", force: :cascade do |t|
     t.string "title"
     t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "plans", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
