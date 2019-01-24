40f73cd70c09dd8f86076acdc4c6e9e753ceda39
Aaron Eichhorst <aaroneichhorst@gmail.com>
2019-01-23 22:11:27 -0600

    Updated Pages_Controller and Home / New pages with membership params

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @basic_plan = Plan.find(1)
 
   end
+
+  def new
+    @pro_plan = Plan.find(2)
+  end
+
 
 
   def about
---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,6 @@
+ class RegistrationsController < Devise::RegistrationsController
+  def new
+    @pro_plan = Plan.find(2)
+  end
+
+end
