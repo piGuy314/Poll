c8f97ddd6eaff72a734e5bd686e124c156f221a6
Aaron Eichhorst <aaroneichhorst@gmail.com>
2019-01-23 22:49:03 -0600

    Updated membership sign up forms

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
 class PagesController < ApplicationController
   def home
     @basic_plan = Plan.find(1)
-
+    @pro_plan = Plan.find(2)
   end
 
   def new
-    @pro_plan = Plan.find(2)
+
   end
 
 
