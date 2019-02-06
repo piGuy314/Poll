27505493387d8a5129610f96d085e66577cc8192
Aaron Eichhorst <aaroneichhorst@gmail.com>
2019-02-05 21:58:17 -0600

    Added routes for user profiles

--- db/migrate/20190124045411_create_profiles.rb
+++ db/migrate/20190124045411_create_profiles.rb
@@ -5,8 +5,9 @@ class CreateProfiles < ActiveRecord::Migration[5.1]
       t.string :first_name
       t.string :last_name
       t.string :gender
+      t.string :age
       t.string :zip_code
-      t.string :contact_email
+      t.string :user_name
       t.text   :description
       t.timestamps
     end
