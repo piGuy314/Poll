444a1592473019d7861c9f65e47b9e45af17fa83
Aaron Eichhorst <aaroneichhorst@gmail.com>
2019-02-05 21:41:28 -0600

    Added profile and user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   belongs_to :plan
+  has_one :profile
 
   attr_accessor :stripe_card_token
   #If Pro users passes validations (email, password, etc.),
   #then call Stripe and tell Stripe to set up a subscription
   #upon charging customer's card.
   #Strip responds back with customer data.
-  #Store customer.id as customer token and save the user. 
+  #Store customer.id as customer token and save the user.
   def save_with_subscription
     if valid?
       customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
