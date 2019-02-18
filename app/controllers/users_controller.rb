dd8404cb137ab91d611882178172f69217e288cb
Aaron Eichhorst <aaroneichhorst@gmail.com>
2019-02-17 22:08:03 -0600

    Working on community index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-
+    @users = User.all
   end
-  
+
 # GET to /users/:id
   def show
     @user = User.find( params[:id] )
