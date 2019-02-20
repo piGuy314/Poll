d6f532417892abd9abf4529b1ee98f4e2ab6c59e
Aaron Eichhorst <aaroneichhorst@gmail.com>
2019-02-19 21:22:25 -0600

    reviewed git branches

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,56 +1,80 @@
 class ProfilesController < ApplicationController
-  before_action :authenticate_user!
-  before_action :only_current_user
 
-  #GET to /users/:user_id/profile/new
-  def new
-    # When action gets run Render blank profiles details form
-    @profile = Profile.new
-  end
+    # Do this before any of these actions
+    # authenticate user is from devise gem
+    before_action :authenticate_user!
 
-  # POST to /users/:user_id/profile
-  def create
-    #Ensure that we have the user who is filling out form
-    @user = User.find( params[:user_id] )
-    #Create profile linked to this specific user
-    @profile = @user.build_profile( profile_params )
-    if @profile.save
-      flash[:success] = "Profile updated!"
-      redirect_to @user
-    else
-      render action: :new
+    before_action :only_current_user
+
+    # Makes takes user to a blank profile form
+    # GET to /users/:user_id/profile/new
+    def new
+      # Render blank profile details form
+      @profile = Profile.new
     end
-  end
 
-# GET to /users/:user_id/profile/edit
-  def edit
-    @user = User.find( params[:user_id] )
-    @profile = @user.profile
-  end
+    # Save it to the profile DB
+    # POST to /users/:user_id/profile
+    def create
+      # Ensure that we have the user who is filling out form
+      @user = User.find(params[:user_id])
 
-# PUT to /users/:user_id/profile
-def update
-  # Retrieve the user from the database
-  @user = User.find( params[:user_id] )
-  # Retrieve that user's profile
-  @profile = @user.profile
-  # Mass assign edited profile attributes and save (update)
-  if @profile. update_attributes(profile_params)
-    flash[:success] = "Profile updated!"
-    #Redirect user to their profile page
-    redirect_to user_path(id: params[:user_id])
-  else
-    render action: :edit
-  end
-end
+      # This will link the profile to the user that is logged in
+      @profile = @user.build_profile(profile_params)
 
-  private
-  def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :avatar, :gender, :zip_code, :contact_email, :description)
-  end
+      # Show message is profile is updated successfully
+      if @profile.save
+        flash[:success] = "Profile updated!"
+        # Send to the user profile page
+        redirect_to user_path(params[:user_id])
+      else
+        # Refresh the page with a blank form
+        render action: :new
+      end
+    end
 
-  def only_current_user
-    @user = User.find( params[:user_id] )
-    redirect_to(root_url) unless @user == current_user
-  end
+    # GET to /users/:user_id/profile/edit
+    def edit
+      # Reach into the database and find that particular user
+      @user = User.find(params[:user_id])
+
+      # Grab that profile
+      @profile = @user.profile
+    end
+
+    # PUT/PATCH request to /users/:user_id/profile
+    def update
+      @user = User.find(params[:user_id])
+
+      @profile = @user.profile
+
+      # Update attributes mass assigns all of the form fields
+      # If the update succeeds, flash a message and send back
+      # to their profile page
+      if @profile.update_attributes(profile_params)
+        flash[:success] = "Profile updated!"
+        redirect_to user_path(id: params[:user_id])
+
+      # If it fails, refresh the edit page
+      else
+        render action :edit
+      end
+    end
+
+
+    private
+      # Whitelist the form fields to prevent hackers from
+      # adding additional form fields
+      def profile_params
+        params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+      end
+
+      # Prevents the user from editing other people's pages
+      def only_current_user
+        @user = User.find(params[:user_id])
+
+        # current_user is from devise
+        redirect_to(root_url) unless @user == current_user
+      end
+
 end
