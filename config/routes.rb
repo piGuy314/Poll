d86dc1c47859a259b55903e054d2534999088e2c
Aaron Eichhorst <aaroneichhorst@gmail.com>
2019-02-25 16:24:22 -0600

    updated routes files for user post creation

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users do
     resource :profile
-    resources :polls
+    resources :polls do
+    resource :poll
   end
+end
 
   get 'about', to: 'pages#about'
   resources :contacts, only: :create
