b7f0f9f7e3598c1a6439b1283aad903221efc833
Aaron Eichhorst <aaroneichhorst@gmail.com>
2019-03-10 22:56:55 -0500

    Updated plan association with header signup link

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,8 +3,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #users signing up with the Pro account (plan ID 2)
   #save with a special Stripe subscription function.
   #Otherwise Devise signs up user as usual.
+  # Only do this when the user is signing up
 
-  
+
+  # Extends the default Devisor behaviour so that
+  # users who are signing up with the pro account is
+  # saved with the special stripe function otherwise,
+  # devise signs up as usual.
+
   def create
     super do |resource|
       if params[:plan]
