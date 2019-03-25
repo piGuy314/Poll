391adb449f01ce3b19f5aea424a5ba6048b916d0
Aaron Eichhorst <aaroneichhorst@gmail.com>
2019-03-24 21:28:26 -0500

    Heroku fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require_tree .
-
 //= require jquery
 //= require jquery_ujs
+//= require_tree .
+
+
 //= require turbolinks
 //= require bootstrap-sprockets
